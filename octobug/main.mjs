c8d9b0a6a2645bb936133112d56c88b327f1613c
top-ghost <top-ghost@users.noreply.github.com>
2024-09-22 21:29:30 +0000

    deploy: 0a04786fe03512ad543636acaace4c620abbd2ba

--- octobug/main.mjs
+++ octobug/main.mjs
@@ -82,33 +82,4 @@ window.addEventListener("DOMContentLoaded", async () => {
       console.log("API not initialized");
     }
   });
-
-
-  window.secretInputs = {};
-  window.secretFields = ['githubPrivateKey', 'githubAppId', 'githubAppInstallationId', 'githubRepoUrl', 'postAuthor']
-  
-  window.secretFields.forEach((fieldName) => {
-    window.secretInputs[fieldName] = document.getElementById(`${fieldName}Input`);
-  })
-  
-  const secretsUpdate = document.getElementById("secretsUpdateButton")
-
-  secretsUpdate.addEventListener("click", async (e) => {
-    e.preventDefault();
-
-    window.secretFields.forEach((fieldName) => {
-      switch (fieldName) {
-        case 'githubRepoUrl':
-          const [_, githubRepoOwner, githubRepoName] = window.secretInputs['githubRepoUrl']?.value?.match(/^https?\:\/\/github\.com\/([^\/]+)\/(.+)$/)
-          localStorage.setItem('githubRepoOwner', githubRepoOwner);
-          localStorage.setItem('githubRepoName', githubRepoName);
-          break;
-        default:
-          localStorage.setItem(fieldName, window.secretInputs[fieldName]?.value);
-      }
-      
-    })
-
-    authorize();
-  });
 });
