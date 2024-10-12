4a759b61e3beb5888d3aa56906f5f6647f3573eb
topghost <topghost@fastmail.com>
2024-10-11 23:42:08 -0700

    put site name in header

--- _data/metadata.js
+++ _data/metadata.js
@@ -4,7 +4,7 @@ module.exports = {
   language: "en",
   description: "@topghost's post-cohost post host, built with octobug",
   author: {
-    name: "topghost",
+    name: "@topghost",
     email: "topghost@fastmail.com",
     url: "https://topposts.net",
   },
